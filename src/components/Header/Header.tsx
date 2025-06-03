d931b0f77652ef1d2506d56c575539668bb8b39c
Renato Teixeira <renato.gfteixeira@outlook.com>
2025-06-03 11:57:46 -0300

    feat: add Header component with responsive navigation menus

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import logo from '../../assets/images/logo.svg';
+import { Menu, X } from 'lucide-react';
+import NavMenuDesktop from '../NavMenu/desktop/NavMenu';
+import NavMenuMobile from '../NavMenu/mobile/NavMenu';
+import { MAIN_NAV_LINKS } from '../../constants/navLinks.ts';
+
+function Header() {
+  const [navOpen, setNavOpen] = useState(false);
+
+  return (
+    <header className={`fixed top-0 z-10 w-full bg-white/80 shadow-sm backdrop-blur-sm`}>
+      <div className="container mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between py-3">
+          <a href="#" className="focus:ring-brown-300 focus:ring-2 focus:outline-none">
+            <img src={logo} alt="Logo Mulher360" className="size-12" />
+          </a>
+
+          <div className="flex items-center justify-center md:hidden">
+            <button
+              onClick={() => setNavOpen(!navOpen)}
+              className="text-brown-500 hover:text-brown-300 focus:ring-brown-300 rounded-md transition-all hover:cursor-pointer focus:ring-2 focus:outline-none"
+              aria-label="Menu"
+            >
+              {navOpen ? <X size="28" /> : <Menu size="28" />}
+            </button>
+          </div>
+
+          <NavMenuDesktop navLinks={MAIN_NAV_LINKS} />
+        </div>
+
+        <NavMenuMobile isOpen={navOpen} navLinks={MAIN_NAV_LINKS} />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
