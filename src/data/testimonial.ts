42dc3c7b46197efd9eab2381b121b7f1c53ec99f
Renato Teixeira <renato.gfteixeira@outlook.com>
2025-06-25 21:38:11 -0300

    fix: add swiper dependency for enhanced carousel functionality

---
+++ src/data/testimonial.ts
@@ -0,0 +1,16 @@
+export const testimonials = [
+  {
+    name: 'Cris Gomes',
+    age: '52',
+    avatar: '/avatars/testimonial-1.svg',
+    content:
+      'Depois de tantas tentativas frustradas de emagrecimento, finalmente encontrei equilíbrio com a ajuda da Dra. Luciana. Aprendi que cuidar da alimentação não é sobre restrições, mas sobre consciência e bem-estar especialmente agora, na menopausa. O acompanhamento dela tem sido essencial para eu me sentir mais saudável, disposta e confiante nessa nova fase. Sou muito grata por todo o cuidado e orientação!',
+  },
+  {
+    name: 'Liliana Souza',
+    age: '53',
+    avatar: '/avatars/testimonial-2.svg',
+    content:
+      'Tenho 53 anos e, com a chegada da menopausa, ganhei muito peso e enfrentei dificuldades para eliminá-lo, mesmo reduzindo a alimentação. Sentia os incômodos típicos dessa fase e nada parecia funcionar. Com o plano alimentar anti-inflamatório da Dra. Luciana, usando comida de verdade e ingredientes do dia a dia, consegui eliminar 3,2 kg em menos de um mês, sem sacrifícios. Além da perda de peso, os sintomas da menopausa melhoraram muito. Para mim, está sendo uma vitória contínua, com mais disposição e qualidade de vida. Só gratidão! ',
+  },
+];
