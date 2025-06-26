22caa5fdea371d70bf258f11e20bde310f186d45
Renato Teixeira <renato.gfteixeira@outlook.com>
2025-06-25 21:38:47 -0300

    feat: add TestimonialCard component with types

---
+++ src/components/TestimonialCard/TestimonialCard.tsx
@@ -0,0 +1,24 @@
+import type { TestimonialCardTypes } from './TestimonialCard.types.ts';
+
+function TestimonialCard({ content, name, age, avatar }: TestimonialCardTypes) {
+  return (
+    <div className="border-brown-300/40 flex w-full flex-col justify-between gap-2 rounded-2xl border bg-white p-6 shadow-lg md:max-w-lg">
+      <p className="line-clamp-13 h-80">{content}</p>
+
+      <div>
+        <hr className="border-brown-500/30 mb-4" />
+
+        <div className="flex items-center gap-2">
+          <img src={avatar} alt="Testimonial" className="size-12 rounded-full" />
+
+          <div>
+            <h4 className="text-lg font-bold">{name}</h4>
+            <p className="text-brown-700/50 -mt-2 text-sm">{age} anos</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TestimonialCard;
