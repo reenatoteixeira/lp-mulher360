10c14d5a426050f8affd77278bd24e8548f36233
Renato Teixeira <renato.gfteixeira@outlook.com>
2025-07-06 09:03:27 -0300

    feat: add LandingPage component with sections for Header, Hero, About, HowItWork, Testimonial, and Methodology

---
+++ src/pages/landing-page/LandingPage.tsx
@@ -0,0 +1,21 @@
+import Header from "../../components/Header/Header";
+import Hero from "./sections/Hero";
+import About from "./sections/About";
+import HowItWork from "./sections/HowItWork";
+import Testimonial from "./sections/Testimonial";
+import Methodology from "./sections/Methodology";
+
+function LandingPage() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <HowItWork />
+      <Testimonial />
+      <Methodology />
+    </>
+  )
+}
+
+export default LandingPage;
